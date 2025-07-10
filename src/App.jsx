06134a293d93024e8bcd5fcd68678d6f96fca6e3
zoro-512 <siddhartha2081@gmail.com>
2025-07-10 15:31:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./Component/Header";
+import Main from "./Component/Main";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
---
+++ src/Component/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+    return(
+        <main>
+            <form className="add-ingredient" >
+                <input type="text" placeholder="eg.oregano" aria-label="Add ingredient" />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    )
+}
